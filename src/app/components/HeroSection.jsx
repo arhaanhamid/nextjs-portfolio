09d44dc5d107aed751f64658cebaefa1c74a5f76
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-09-08 17:23:23 +0530

    initial commit

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className="text-white">Hi, Im Arhaan</h1>
+    </section>
+  )
+}
+
+export default HeroSection
