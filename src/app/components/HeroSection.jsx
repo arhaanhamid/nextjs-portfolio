fa5664f84ee92cd423aacafcf411a99e830b0f6e
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-09-08 17:36:27 +0530

    First Commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
     <section>
-        <h1 className="text-white">Hi, Im Arhaan</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hi, Im Arhaan!
+          </h1>
+          <p className="text-[#ADB7BE] text-lg lg:text-xl mb-6">
+            Im a software engineer based in New York. Im passionate about
+            building beautiful and intuitive web applications.
+          </p>
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-white text-black hover:bg-slate-200">
+              Hire Me
+            </button>
+            <button className="px-6 py-3 rounded-full mr-4 bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
+              Download CV
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="avatar"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
