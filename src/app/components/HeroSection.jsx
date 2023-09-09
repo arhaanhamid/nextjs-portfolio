8ed588a8978e7c2b33711684e269da0f28d0ea06
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-09-09 15:12:48 +0530

    Navbar complete

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,24 +1,48 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hi, Im Arhaan!
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl am:Text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hi, Im{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Arhaan",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Web Developer",
+                1000,
+                "Mobile Developer",
+                1000,
+                "UI/UX Designer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              // style={{ fontSize: "2em", display: "inline-block" }}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl mb-6">
+          <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
             Im a software engineer based in New York. Im passionate about
             building beautiful and intuitive web applications.
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white text-black hover:bg-slate-200">
+            <button className="px-6 py-3 rounded-full sm:w-fit mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-white text-white hover:bg-slate-200">
               Hire Me
             </button>
-            <button className="px-6 py-3 rounded-full mr-4 bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
-              Download CV
+            <button className="px-1 py-1 rounded-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-whit mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
             </button>
           </div>
         </div>
