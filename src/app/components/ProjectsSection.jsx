6db73f3423d9f945cc49c41c074048adef42c5ea
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-12-13 16:06:27 +0530

    added new projects and changed thumbnails

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -5,20 +5,20 @@ import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
 
 const projectsData = [
-  {
-    id: 1,
-    title: "Portfolio",
-    description: "Personal Portfolio",
-    image: "/images/projects/1.png",
-    tag: ["All", "Web"],
-    gitUrl: "https://github.com/arhaanhamid/nextjs-portfolio",
-    previewUrl: "https://github.com/arhaanhamid",
-  },
+  // {
+  //   id: 1,
+  //   title: "Portfolio",
+  //   description: "Personal Portfolio",
+  //   image: "/images/projects/1.png",
+  //   tag: ["All", "Web"],
+  //   gitUrl: "https://github.com/arhaanhamid/nextjs-portfolio",
+  //   previewUrl: "https://github.com/arhaanhamid",
+  // },
   {
     id: 2,
     title: "Share Prompts",
     description: "Find and Share exceptional prompts",
-    image: "/images/projects/2.png",
+    image: "/images/projects/share_prompts.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/arhaanhamid/share-prompts",
     previewUrl: "https://share-prompts-indol.vercel.app",
@@ -27,7 +27,7 @@ const projectsData = [
     id: 3,
     title: "Solo Leveling",
     description: "Top-Down Prespective Action Game",
-    image: "/images/projects/2.png",
+    image: "/images/projects/solo_leveling.png",
     tag: ["All", "Application"],
     gitUrl: "https://github.com/arhaanhamid/sololeveling_javafx_game",
     previewUrl: "/",
@@ -36,7 +36,7 @@ const projectsData = [
     id: 4,
     title: "Quiz Me",
     description: "An quiz/trivia webapp to test your knowledge",
-    image: "/images/projects/2.png",
+    image: "/images/projects/quiz_me.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/arhaanhamid/quiz-me_react",
     previewUrl: "https://quiz-me-react.vercel.app/",
@@ -45,7 +45,7 @@ const projectsData = [
     id: 5,
     title: "Tenzies",
     description: "Tenzies based game made using react",
-    image: "/images/projects/2.png",
+    image: "/images/projects/tenzies_game.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/arhaanhamid/tenzies-game_react",
     previewUrl: "https://tenzies-game-react-ochre.vercel.app/",
@@ -54,10 +54,66 @@ const projectsData = [
     id: 6,
     title: "BlackBoxes",
     description: "A simple webapp puzzle game made using react",
-    image: "/images/projects/2.png",
+    image: "/images/projects/black_boxes.png",
     tag: ["All", "Web"],
-    gitUrl: "https://github.com/arhaanhamid/quiz-me_react",
+    gitUrl: "https://github.com/arhaanhamid/blackboxes-game_react",
     previewUrl: "https://blackboxes-game-react.vercel.app/",
+  },
+  {
+    id: 7,
+    title: "Meme Generator",
+    description:
+      "Generate your own memes using this meme generator made using react",
+    image: "/images/projects/meme_generator.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/arhaanhamid/Meme-Generator_react",
+    previewUrl: "https://meme-generator-react-psi.vercel.app/",
+  },
+  {
+    id: 8,
+    title: "URL Shortener",
+    description:
+      "Shorten your long urls for free and keep them easy to remember.",
+    image: "/images/projects/api.png",
+    tag: ["All", "Web", "Microservices"],
+    gitUrl: "https://github.com/arhaanhamid/url-shortener-microservice",
+    previewUrl: "https://url-shortener-k8ej.onrender.com/",
+  },
+  {
+    id: 9,
+    title: "Exercise Tracker",
+    description: "Track you daily exercise and keep a log of them.",
+    image: "/images/projects/api.png",
+    tag: ["All", "Web", "Microservices"],
+    gitUrl: "https://github.com/arhaanhamid/exercise-tracker-microservice",
+    previewUrl: "https://exercise-tracker-microservice-if04.onrender.com/",
+  },
+  {
+    id: 10,
+    title: "File Metadata",
+    description: "Upload a file and get its metadata.",
+    image: "/images/projects/api.png",
+    tag: ["All", "Web", "Microservices"],
+    gitUrl: "https://github.com/arhaanhamid/file-metadata-microservice",
+    previewUrl: "https://file-metadata-microservice-p536.onrender.com/",
+  },
+  {
+    id: 11,
+    title: "Request Header Parser",
+    description: "Get the header data of any request.",
+    image: "/images/projects/api.png",
+    tag: ["All", "Web", "Microservices"],
+    gitUrl: "https://github.com/arhaanhamid/request-header-parser-microservice",
+    previewUrl: "https://request-header-parser-microservice-cjqj.onrender.com/",
+  },
+  {
+    id: 12,
+    title: "TImestamp Microservice",
+    description: "Write any date in the url and get its proper timestamp.",
+    image: "/images/projects/api.png",
+    tag: ["All", "Web", "Microservices"],
+    gitUrl: "https://github.com/arhaanhamid/timestamp-microservice",
+    previewUrl: "https://timestamp-microservice-85un.onrender.com/",
   },
 ];
 
@@ -85,7 +141,7 @@ const ProjectsSection = () => {
         My Projects
       </h2>
 
-      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+      <div className="text-white flex flex-wrap flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
           onClick={handleTagChange}
           name="All"
@@ -100,6 +156,11 @@ const ProjectsSection = () => {
           onClick={handleTagChange}
           name="Application"
           isSelected={tag === "Application"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Microservices"
+          isSelected={tag === "Microservices"}
         />
       </div>
 
